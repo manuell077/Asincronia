95577a081922311d277dcec7aa73381cae874142
unknown <manuell077>
2025-03-04 19:16:40 -0500

    Ejercicio 12

--- JavaScript/Ejercicio11.js
+++ JavaScript/Ejercicio11.js
@@ -1,10 +1,10 @@
 function Promisa(){
     
-    return new Promise ((resolve,reject) => {
+    return new Promise ((resolve,reject) => { //Creacion de la promesa 
 
-       setTimeout(() => {
+       setTimeout(() => { //Imprimira despues 3 segundos 
             
-        resolve("Promisa resuelta");
+        resolve("Promisa resuelta"); //si se resuelve
 
        },3000)
 
@@ -13,4 +13,4 @@ function Promisa(){
 
 }
 
-Promisa().then((resultado) => alert(resultado) );
+Promisa().then((resultado) => alert(resultado) ); //con then obtenemos el resultado y lo imprimimos en pantalla con un alert 
---
+++ JavaScript/Ejercicio12.js
@@ -0,0 +1,28 @@
+/*Define una función asincrónica que espere 1 segundo y luego devuelva una cadena que
+diga "Operación completada". Utiliza async/await. */
+
+function Promesa(){ //Se crea la promesa que se resuelve
+   return new Promise ((resolve,reject) => {
+         
+
+    setTimeout( () => {  
+       resolve("Operacion completada");
+    },1000)
+
+   } )
+
+
+}
+
+
+
+async function asincronia() { //Se crea metodo asincronico que espera con el await a que se resuelva la promesa 
+      
+    let promise = await Promesa();
+    alert(promise);
+    
+
+    
+}
+
+asincronia();
